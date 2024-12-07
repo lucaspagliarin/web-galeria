6961c91be8ff96a158435ce14b0ba1d569147b0b
laureen <germani.laureen@gmail.com>
2024-12-07 15:58:46 -0300

    adição de login, cadastro e logout do usuário

--- galeria/forms.py
+++ galeria/forms.py
@@ -5,5 +5,53 @@ class FormAdiciona(forms.Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.fields['tags'] = forms.CharField(max_length=500, label='tags', required=False)
+        self.fields['tags'] = forms.CharField(max_length=500, label='Tags', required=False)
+        self.fields['collection'] = forms.CharField(max_length=500, label='Collection', required=False)
+        self.fields['favorite'] = forms.BooleanField(required=False, initial=False)
 
+
+class FormLogin(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, *kwargs)
+
+        self.fields['username'] = forms.CharField(max_length=150, label='Username', required=True)
+        self.fields['username'].widget.attrs.update({
+            'placeholder': 'Email or Username',
+            'autocomplete': 'off',
+        })
+        self.fields['password'] = forms.CharField(max_length=500, label='Password', required=True, widget=forms.PasswordInput)
+        self.fields['password'].widget.attrs.update({
+            'placeholder': 'Password',
+            'autocomplete': 'off',
+        })
+
+
+class FormCadastro(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['username'] = forms.CharField(max_length=150, label='Username', required=True)
+        self.fields['username'].widget.attrs.update({
+            'placeholder': 'Username',
+            'autocomplete': 'off',
+        })
+        self.fields['password'] = forms.CharField(max_length=500, label='Password', required=True, widget=forms.PasswordInput)
+        self.fields['password'].widget.attrs.update({
+            'placeholder': 'Password',
+            'autocomplete': 'off',
+        })
+        self.fields['first_name'] = forms.CharField(label='First Name', required=True)
+        self.fields['first_name'].widget.attrs.update({
+            'placeholder': 'First Name',
+            'autocomplete': 'off',
+        })
+        self.fields['last_name'] = forms.CharField(label='Last Name', required=True)
+        self.fields['last_name'].widget.attrs.update({
+            'placeholder': 'Last Name',
+            'autocomplete': 'off',
+        })
+        self.fields['email'] = forms.EmailField(required=True)
+        self.fields['email'].widget.attrs.update({
+            'placeholder': 'Email',
+            'autocomplete': 'off',
+        })
--- webgaleria/urls.py
+++ webgaleria/urls.py
@@ -19,5 +19,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('galeria.urls'))
+    path('', include('galeria.urls')),
+    path('adicionar/', include('galeria.urls')),
+    path('login/', include('galeria.urls')),
+    path('cadastro/', include('galeria.urls')),
+    path('logout/', include('galeria.urls')),
 ]
